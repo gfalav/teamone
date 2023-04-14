a4383797f0eed5535565fb3f9d88eacef9512952
Gustavo Falavigna <gfalav@hotmail.com>
2023-04-14 16:22:01 -0300

    appbar w/notification and messages

--- src/comps/layout/topbar/Topbar.jsx
+++ src/comps/layout/topbar/Topbar.jsx
@@ -1,8 +1,10 @@
-import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material"
+import { AppBar, Box, IconButton, Toolbar } from "@mui/material"
 import MenuIcon from '@mui/icons-material/Menu'
 import User from "./User"
 import { useRecoilState, useRecoilValue } from "recoil"
 import { drawerWidth, toggleMenu } from "../../recoil/recoil"
+import Messages from "./Messages"
+import Notification from "./Notification"
 
 const Topbar = () => {
     const dw = useRecoilValue(drawerWidth)
@@ -24,6 +26,12 @@ const Topbar = () => {
                         <MenuIcon />
                     </IconButton>
                     <Box sx={{ flexGrow: 1}} />
+                    <Box  display='flex' flexDirection='row' sx={{
+                        mr: 4
+                    }}>
+                        <Messages />
+                        <Notification />
+                    </Box>
                     <User />
                 </Toolbar>
             </AppBar>
